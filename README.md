5fe013c43109106fcd5e33233fcf12a0b150a8f8
Tarek Sherif <tsherif@gmail.com>
2014-07-24 15:38:11 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+Gaza Data
+=========
+
+Open raw and JSON formatted data documenting the Palestinian and Israeli lives lost in July 2014 attack on Gaza.
+
+Source: http://www.aljazeera.com/news/middleeast/2014/07/gaza-under-seige-naming-dead-2014710105846549528.html
+
+This data is free to use by anyone for any purpose. The code for **parse-data.js** is free to use under the MIT licence.
+
---
+++ parse-data.js
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////////////
+// The MIT License (MIT)
+// 
+// Copyright (c) 2014 Tarek Sherif
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/////////////////////////////////////////////////////////////////////////////////////
+
+"use strict";
+
+var fs = require("fs");
+
+var input = process.argv[2] || "gaza.raw";
+var output = process.argv[3] || (input.replace(/\.raw$/, "") + ".json");
+
+var dead = [];
+
+fs.readFile(input, { encoding: "utf8" }, function(err, data) {
+  if (err) {
+    throw new Error(err);
+  }
+
+  var lines = data.split("\n");
+
+  lines.forEach(function(line) {
+    line = line.trim().split(",");
+
+    var name = line[0] || null;
+    var age = line[1] ? parseFloat(line[1]) : null;
+    var location_of_death = line[2] ? line[2].replace("killed in ", "") : null;
+
+    dead.push({
+      name: name,
+      age: age,
+      location_of_death: location_of_death
+    });
+  });
+
+  fs.writeFile(output, JSON.stringify(dead, null, 2), { encoding: "utf8" }, function(err) {
+    if (err) {
+      throw new Error(err);
+    }
+  });
+});
